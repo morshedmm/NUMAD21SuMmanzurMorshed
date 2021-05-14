588d3b8b1c302c044d4831dff8a11e8eb746d238
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-05-13 21:19:22 -0400

    adding text

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     public void aboutMe(View view) {
 
         //TextView emailView = findViewById(R.id.aboutEmail);
-        //TextView textView = findViewById(R.id.myName);
-        //textView.setText("MManzur Morshed");
+        TextView textView = findViewById(R.id.myName);
+        textView.setText("MManzur Morshed");
         //emailView.setText("morshed.mm@northeastern.edu");
 
     }
