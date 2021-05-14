35073d0d5f57ba4830b8f1a94a411d98224b6f5a
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-05-13 21:18:39 -0400

    adding

---
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.neu.madcourse.NUMAD21SuMmanzurMorshed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aboutMe(View view) {
+
+        //TextView emailView = findViewById(R.id.aboutEmail);
+        //TextView textView = findViewById(R.id.myName);
+        //textView.setText("MManzur Morshed");
+        //emailView.setText("morshed.mm@northeastern.edu");
+
+    }
+}
