075d1ee60702e5ef45e14310d1ad7805cf25472c
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-05-27 12:57:57 -0400

    code cleanup

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/MainActivity.java
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void aboutMe(View view) {
-
-        /*
-        TextView emailView = findViewById(R.id.aboutEmail);
-        TextView textView = findViewById(R.id.myName);
-        textView.setText("MManzur Morshed");
-        emailView.setText("morshed.mm@northeastern.edu");
-
-         */
-
+        
         Intent activity2Intent = new Intent(getApplicationContext(), AboutMeActivity.class);
         startActivity(activity2Intent);
 
