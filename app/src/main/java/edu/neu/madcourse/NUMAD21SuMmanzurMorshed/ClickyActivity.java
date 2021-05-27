012f467542dfd6e554235b44aeea2690c25d2dbe
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-05-27 17:09:21 -0400

    added dummy output for button A

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/ClickyActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ClickyActivity extends AppCompatActivity {
 
@@ -13,6 +14,14 @@ public class ClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+
+    }
+
+    public void clickyResponse(View view) {
+
+        TextView textView = findViewById(R.id.response);
+        textView.setText("MManzur Morshed");
+
 
     }
 }
