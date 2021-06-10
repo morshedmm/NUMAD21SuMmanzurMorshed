300ffb4dd29bb83c940cac7b22e750f784066e4c
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-06-10 13:43:52 -0400

    trying other ways..app showed latitude but crashed

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
@@ -1,20 +1,25 @@
 package edu.neu.madcourse.NUMAD21SuMmanzurMorshed;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.TextView;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements  LocationListener{
 
     private LocationManager locationManager;
     private TextView latitudeValue;
@@ -46,13 +51,110 @@ public class LocationActivity extends AppCompatActivity {
         */
 
         //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 10000, listener);
-        try {
-            final Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            latitudeValue.setText(String.valueOf(lastKnownLocation.getLatitude()));
-        } catch (SecurityException e) {
-            latitudeValue.setText("Not available");
-        }
+        //try {
+        //    final Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //    latitudeValue.setText(String.valueOf(lastKnownLocation.getLatitude()));
+        //} catch (SecurityException e) {
+        //    latitudeValue.setText("Not available");
+        //}
 
+        checkLocationPermission();
 
     }
+    ///////////
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission")
+                        .setMessage("Allow Access?")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(LocationActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 400, 1, this);
+                        Location curLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        //Location curLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (curLocation != null) {
+                            latitudeValue.setText(String.valueOf(curLocation.getLatitude()));
+                        } else{
+                            //latitudeValue.setText("Location null");
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+
+                        }
+                        //latitudeValue.setText(String.valueOf(curLocation.getLatitude()));
+                        //latitudeValue.setText("INSIDE");
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    latitudeValue.setText("NOT granted");
+
+                }
+                return;
+            }
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+    /////
+
+
 }
