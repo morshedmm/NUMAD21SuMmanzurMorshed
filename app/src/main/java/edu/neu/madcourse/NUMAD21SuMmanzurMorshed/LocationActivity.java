4ddeedc44202883e928a2ea2de4bbdfdb4068a8b
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-06-09 21:09:06 -0400

    adding code for latitude and longitude text and variables

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
@@ -1,14 +1,33 @@
 package edu.neu.madcourse.NUMAD21SuMmanzurMorshed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager locationManager;
+    private TextView latitudeValue;
+    private TextView longitudeValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        latitudeValue = (TextView) findViewById(R.id.latitudeValue);
+        longitudeValue = (TextView) findViewById(R.id.longitudeValue);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+
     }
 }
