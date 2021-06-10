caafdb1cdab8d90777c45351cd1b502e8f74c71c
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-06-10 09:08:44 -0400

    trying to implement differently

--- app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD21SuMmanzurMorshed/LocationActivity.java
@@ -2,8 +2,11 @@ package edu.neu.madcourse.NUMAD21SuMmanzurMorshed;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,7 +14,7 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class LocationActivity extends AppCompatActivity implements LocationListener {
+public class LocationActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private TextView latitudeValue;
@@ -28,14 +31,28 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         latitudeValue = (TextView) findViewById(R.id.latitudeValue);
         longitudeValue = (TextView) findViewById(R.id.longitudeValue);
 
+        //gpsLocation = requestUpdatesFromProvider(LocationManager.GPS_PROVIDER, R.string.not_support_gps);
+        /*
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        */
 
-    }
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 10000, listener);
+        try {
+            final Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            latitudeValue.setText(String.valueOf(lastKnownLocation.getLatitude()));
+        } catch (SecurityException e) {
+            latitudeValue.setText("Not available");
+        }
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-
-        latitudeValue.setText(String.valueOf(location.getLatitude()));
-        longitudeValue.setText(String.valueOf(location.getLongitude()));
 
     }
 }
